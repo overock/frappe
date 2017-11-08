e89d24ba3a55254a36f1b13cf611c932a0ae0f03
Roaster Kay <overock@ex-em.com>
2017-11-08 11:07:50 +0900

    oozie 버전별 action list 정리

---
+++ js/defs/versions.js
@@ -0,0 +1,4 @@
+export default {
+    '3.1.3': [  'start', 'end', 'kill', 'decision', 'fork', 'join',
+                'map-reduce', 'pig', 'fs', 'ssh', 'sub-workflow', 'java', 'email', 'custom']
+};
